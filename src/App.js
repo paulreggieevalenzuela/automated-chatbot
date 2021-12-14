04d3bb53b66d72430b7e7895164d7718afe0151a
paulvalenzuela <paul.valenzuela@bcstechnology.com.au>
2021-12-14 16:06:59 +0800

    NEW: Used third party library for the chatbot and integrate wit.ai for the automation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   return (
@@ -18,6 +19,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <MessengerCustomerChat
+        pageId="107781125092013"
+        appId="494289928541104"
+      />
     </div>
   );
 }
